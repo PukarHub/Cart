bedfb674a262e06b2bb63f29c9a6a1417941339c
pukar <pukarupreti3@gmail.com>
2020-09-06 19:31:10 +0545

    dropdown update

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getNumbers } from '../actions/getAction';
+import { Link } from 'react-router-dom';
+import Dropdown from './Dropdown'
+
+const Navbar = (props) => {
+    useEffect(() => {
+        getNumbers();
+    }, []);
+    return (
+        <header>
+            <div className="overlay"></div>
+            <nav>
+                <h2>
+                    <Link to="/">Beauty Shop</Link>
+                </h2>
+                <ul>
+                <li>
+                    <Dropdown />
+                </li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="cart">
+                        <Link to="/cart">
+                            <i className="fas fa-shopping-basket"></i>
+                            Cart <span>{props.basketProps.basketNumbers}</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+const mapStateToProps = (state) => ({
+    basketProps: state.basketState,
+});
+
+export default connect(mapStateToProps, { getNumbers })(Navbar);
