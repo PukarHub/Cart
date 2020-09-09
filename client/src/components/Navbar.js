b7990b4cf9269eb66b03ae9578e69c532f37b657
Your Name <you@example.com>
2020-09-09 19:51:14 +0545

    modal

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,12 +3,27 @@ import { connect } from 'react-redux';
 import { getNumbers } from '../actions/getAction';
 import { Link } from 'react-router-dom';
 import Dropdown from './Dropdown'
+import { Modal, Button } from 'antd';
+import "antd/dist/antd.css";
+import Cart from "./Cart"
 
 const Navbar = (props) => {
     useEffect(() => {
         getNumbers();
     }, []);
+
+    const [visible, setVisible] = React.useState(false)
+
+    const handleClick = () => (
+      setVisible(true)
+    )
+    const handleOk = () => (
+      setVisible(false)
+    )
+
+
     return (
+      <div>
         <header>
             <div className="overlay"></div>
             <nav>
@@ -17,24 +32,35 @@ const Navbar = (props) => {
                 </h2>
                 <ul>
                 <li>
-               
+
                 </li>
                     <li>
                         <Link to="/">Home</Link>
                     </li>
+                    <li>
+                        <Button type="primary" onClick={handleClick}>DEMO</Button>
+                    </li>
                     <li className="cart">
                         <Link to="/">
-                        <i  className="fas fa-shopping-basket"></i> 
+                        <i  className="fas fa-shopping-basket"></i>
                         <button id="btn">
-                        <Dropdown />
-                        <span>{props.basketProps.basketNumbers}</span> 
-                        </button> 
-                            
+                        <span>{props.basketProps.basketNumbers}</span>
+                        </button>
                         </Link>
                     </li>
                 </ul>
             </nav>
         </header>
+
+        <Modal
+          title="Basic Modal"
+          visible={visible}
+          onOk={handleOk}
+        >
+          <Cart />
+        </Modal>
+
+        </div>
     );
 };
 
